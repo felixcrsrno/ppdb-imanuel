0d34fb7f2ebdc9f844b4ce76af4da94a8707cb77
Felix Cris Rino <141736350+felixcrsrno@users.noreply.github.com>
2026-08-15 14:18:21 +0700

    Add database/migrations/2026_08_07_061014_create_student_profiles_table.php

---
+++ database/migrations/2026_08_07_061014_create_student_profiles_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('registration_id')
+                ->constrained('registrations')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->string('nik', 16)->unique();
+            $table->string('full_name');
+            $table->enum('gender', ['L', 'P']);
+            $table->string('birth_place');
+            $table->date('birth_date');
+            $table->text('address');
+            $table->string('parent_name');
+            $table->string('parent_phone');
+            $table->string('parent_job');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student_profiles');
+    }
+};
+
