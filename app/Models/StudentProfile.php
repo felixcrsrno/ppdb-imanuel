b943a74480a58cc015741daad362f5c304876953
Felix Cris Rino <141736350+felixcrsrno@users.noreply.github.com>
2026-08-15 14:16:30 +0700

    Add app/Models/StudentProfile.php

---
+++ app/Models/StudentProfile.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StudentProfile extends Model
+{
+    protected $fillable = [
+        'registration_id',
+        'nik',
+        'full_name',
+        'gender',
+        'birth_place',
+        'birth_date',
+        'address',
+        'parent_name',
+        'parent_phone',
+        'parent_job',
+    ];
+}
+
