016184d3efca4ada6d62220e11da5bbc12243a11
Felix Cris Rino <141736350+felixcrsrno@users.noreply.github.com>
2026-08-15 14:16:28 +0700

    Add app/Models/User.php

---
+++ app/Models/User.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class User extends Authenticatable
+{
+    protected $fillable = ['name', 'email', 'password', 'role'];
+
+    public function registration(): HasOne
+    {
+        return $this->hasOne(Registration::class);
+    }
+}
+
